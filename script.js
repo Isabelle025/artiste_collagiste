561db005b412a79662c56f5e83921ded251aade5
Isabelle025 <isagrangeret@gmail.com>
2022-06-14 11:28:38 +0200

    partie biographie terminée et menu survol mis en place

--- script.js
+++ script.js
@@ -1,32 +1,20 @@
-const date1 = document.querySelector('.date1')
-date1.onclick = function () {
-    show('.text1');
-}
-
-const date2 = document.querySelector('.date2')
-date2.onclick = function () {
-    show('.text2');
-}
-
-const date3 = document.querySelector('.date3')
-date3.onclick = function () {
-    show('.text3');
-}
-
-const date4 = document.querySelector('.date4')
-date4.onclick = function () {
-    show('.text4');
-}
-
-function show(bio) 
-	{
-		var text = document.querySelector(bio);
- 
-        if (text.style.opacity == '1'){
-            text.style.opacity = ('0');
-        } else {
-            text.style.opacity = ('1');
+/*changement d'image de fond de l'entête lorsqu'on clique sur le menu, apparition de l'entête avec toutes les rubriques cliquables pour se déplacer dans les différentes partie du site et disparition du nom de l'artiste*/
+const menu = document.querySelector('.menu')
+menu.onclick = function () {
+    // nav('.background_menu');
+    nav('.category_menu');
+    document.querySelector('.name').style.display= "none";
+    document.querySelector('.menu').style.display="none";
+    }
+    
+function nav(entry) 
+        {
+            var text = document.querySelector(entry);
+     
+            if (text.style.display == 'grid'){
+                text.style.display = ('unset');
+            } else {
+                text.style.display = ('grid');
+            }
+            
         }
-        
-	}
- 
