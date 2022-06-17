8949d941899de7b5ed6d1babc3e1ecbf622d1770
Isabelle025 <isagrangeret@gmail.com>
2022-06-17 15:22:03 +0200

    mise à jour du menu nav

--- script.js
+++ script.js
@@ -18,3 +18,18 @@ function nav(entry)
             }
             
         }
+
+/*partie pour la gallerie des oeuvres
+        const panels=document.querySelectorAll('.panel')
+        panels.forEach((panel)=>{//loop on every cards
+            console.log(panel)
+          panel.addEventListener("click",()=>{//add a click event
+            removeActiveClasses()
+            panel.classList.add("active");//when i click i add a class style 'active '
+          })//
+        })
+        function removeActiveClasses(){
+            panels.forEach((panel)=>{//when i click in other cards i remove active class on the others panel
+                panel.classList.remove("active")
+            })
+        }*/
