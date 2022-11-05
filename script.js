18ee09a2c903175e66a37fb4846d161b642b4f26
Isabelle025 <isagrangeret@gmail.com>
2022-11-05 16:47:56 +0100

    correction galerie

--- script.js
+++ script.js
@@ -187,3 +187,159 @@ rubric4.addEventListener('mouseout',function()
     catbio8.style.color = "#000000"
 });
 
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~messages erreurs formulaire newsletter~~~~~~~~~~~~~~~~~~~~~~~~
+const form = document.querySelector('#signup');
+
+const checkUsername = () => {
+
+    let valid = false;
+
+const username = document.getElementById('name');
+
+    if (username.value=="") {
+        showError(username, 'Merci de saisir un nom');
+    }else {
+        showSuccess(username);
+        valid = true;
+    }
+    return valid;
+};
+
+const emailEl = document.querySelector('#email');
+
+const checkEmail = () => {
+    let valid = false;
+
+    const email = emailEl.value.trim();
+    if (!isRequired(email)) {
+        showError(emailEl, 'Merci de saisir un email');
+    } else if (!isEmailValid(email)) {
+        showError(emailEl, 'Cet email est invalide')
+    } else {
+        showSuccess(emailEl);
+        valid = true;
+    }
+    return valid;
+};
+
+const isEmailValid = (email) => {
+    const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(email);
+};
+
+const checkSujet = () => {
+
+    let valid = false;
+
+const sujet_error = document.getElementById('sujet');
+
+    if (sujet_error.value=="") {
+        showError(sujet_error, 'Merci de saisir un sujet');
+    }else {
+        showSuccess(sujet_error);
+        valid = true;
+    }
+    return valid;
+
+};
+
+const checkMsg = () => {
+
+    let valid = false;
+
+
+const msg_error = document.getElementById('msg');
+
+    if (msg_error.value=="") {
+        showError(msg_error, 'Merci de saisir un message');
+    }else {
+        showSuccess(msg_error);
+        valid = true;
+    }
+    return valid;
+
+};
+
+
+const isRequired = value => value === '' ? false : true;
+const isBetween = (length, min, max) => length < min || length > max ? false : true;
+
+
+const showError = (input, message) => {
+    // get the form-field element
+    const formField = input.parentElement;
+    // add the error class
+    formField.classList.remove('success');
+    formField.classList.add('error');
+
+    // show the error message
+    const error = formField.querySelector('small');
+    error.textContent = message;
+};
+
+const showSuccess = (input) => {
+    // get the form-field element
+    const formField = input.parentElement;
+
+    // remove the error class
+    formField.classList.remove('error');
+    formField.classList.add('success');
+
+    // hide the error message
+    const success = formField.querySelector('small');
+    success.textContent = '';
+}
+
+
+form.addEventListener('submit', function (e) {
+    // prevent the form from submitting
+    e.preventDefault();
+
+    // validate fields
+    let isUsernameValid = checkUsername(),
+        isEmailValid = checkEmail(),
+        isSujetValid = checkSujet(),
+        isMsgValid = checkMsg();
+
+    let isFormValid = isUsernameValid &&
+        isEmailValid &&
+        isSujetValid &&
+        isMsgValid;
+
+    // submit to the server if the form is valid
+    if (isFormValid) {
+
+    }
+});
+
+
+const debounce = (fn, delay = 500) => {
+    let timeoutId;
+    return (...args) => {
+        // cancel the previous timer
+        if (timeoutId) {
+            clearTimeout(timeoutId);
+        }
+        // setup a new timer
+        timeoutId = setTimeout(() => {
+            fn.apply(null, args)
+        }, delay);
+    };
+};
+
+form.addEventListener('input', debounce(function (e) {
+    switch (e.target.id) {
+        case 'username':
+            checkUsername();
+            break;
+        case 'email':
+            checkEmail();
+            break;
+        case 'sujet_error':
+            checkSujet();
+            break;
+        case 'msg_error':
+            checkMsg();
+            break;
+    }
+}));
